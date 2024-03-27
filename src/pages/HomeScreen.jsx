00b74de99ae56d94f299e4878439a762d5f69357
arfakhan <khanarfa8879@gmail.com>
2024-03-27 20:59:57 +0530

    adding loading and error handling

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
 import ProductList from "../components/ProductList";
 import ProductListSkeleton from "../components/ProductListSkeleton";
+import ErrorScreen from "./ErrorScreen";
 
 export default function HomeScreen() {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+  const [isloading, setLoading] = useState(true);
+  const [iserror, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function getAllProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.products))
-      .catch((error) => console.error("Error fetching products:", error));
-  }
+  const getAllProducts = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data.products);
+      } else {
+        setError(true);
+        setErrorMessage("Error fetching products: " + response.status);
+      }
+    } catch (error) {
+      setError(true);
+      setErrorMessage("Error fetching products: " + error.message);
+      
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     getAllProducts();
@@ -18,11 +34,9 @@ export default function HomeScreen() {
 
   return (
     <div>
-      {products ? (
-        <ProductList products={[...products]} />
-      ) : (
-        <ProductListSkeleton />
-      )}
+      {isloading && <ProductListSkeleton />}
+      {products.length > 0 && <ProductList products={[...products]} />}
+      {iserror && <ErrorScreen errorMessage={errorMessage}/>}
     </div>
   );
 }
